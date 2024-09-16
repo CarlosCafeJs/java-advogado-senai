35e5849d27646e0ee226a2ba41a4e6148322761f
Carlos Henrique dos Santos <129197782+CarlosCafeJs@users.noreply.github.com>
2024-09-16 18:01:04 +0000

    Adicionando primeiro codigo.

---
+++ advocacia/src/classesAbstratas/Entidades.java
@@ -0,0 +1,13 @@
+public abstract class Pessoa {
+  private String nome;
+  private String CPF;
+  private String CNPJ;
+  private String email;
+  private String documento;
+  private String numeroContato;
+
+
+
+
+
+}
